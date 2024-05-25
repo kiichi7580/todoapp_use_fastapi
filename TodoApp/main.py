8f6e379438b9de19ffe71ec2ac2df7d684605046
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-05-25 20:43:21 +0900

    ルートにadminとusersを追加

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import auth, todos
+from routers import auth, todos, admin, users
 import models
 from database import engine
 
@@ -10,3 +10,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
